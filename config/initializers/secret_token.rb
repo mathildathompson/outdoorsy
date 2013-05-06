e038dc47706610029cef852a175e79027638f734
Chris Wong <chwongris@gmail.com>
2013-05-06 18:57:40 -0400

    skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Outdoorsy::Application.config.secret_token = '297f32669c5795ed7ed53d2513b089210530825eb7d8c38839c9ec020e545d2913455fca1b9b0d865b2bb537c8aa0e635fa46de870c0a5d4287a7338cbc5e8ff'
