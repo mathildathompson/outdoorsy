40fe6b2e050ee7e4b928faa30c4dded61641dd47
Mathilda Thompson <mathildathompson288@Mathildas-MacBook-Pro.local>
2013-05-08 00:10:55 +0100

    got basic devise working

--- config/routes.rb
+++ config/routes.rb
@@ -50,8 +50,10 @@ Outdoorsy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
+  #root :to => 'users#sign_in'
+devise_scope :user do
+  root to: "devise/sessions#new"
+end
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
