6f732560b2f301767486ba863b1917ef6162f863
Jen Lim <jennolim@gmail.com>
2013-05-08 17:28:11 -0400

    Add wifi seed method and populated dev db with wifi locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130508025005) do
+ActiveRecord::Schema.define(:version => 20130508212246) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -100,6 +100,9 @@ ActiveRecord::Schema.define(:version => 20130508025005) do
   create_table "wifis", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "location"
+    t.string   "name"
+    t.string   "address"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,14 @@ json["data"].each do |a|
 		:longitude => a[16][2])
 end
 
+json = ActiveSupport::JSON.decode(File.read('public/manhattanwifi.json')) 
+json["data"].each do |a| 
+	Wifi.create!(:name => a[10], 
+		:location => a[9], 
+		:address => a[15][0])
+end 
+
+
 
 
   	# @newpark = OutdoorArea.new 
