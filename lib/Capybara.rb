f00a9d076571f15fe961a4716e45b6a9225b329b
Chris Wong <chwongris@gmail.com>
2013-05-16 13:48:33 -0400

    modified gemfile

--- lib/Capybara.rb
+++
@@ -1,6 +0,0 @@
-Capybara.configure do |config|
-      config.match = :one
-      config.exact_options = true
-      config.ignore_hidden_elements = true
-      config.visible_text_only = true
-    end
